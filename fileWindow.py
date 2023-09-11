d379e906f60ba214a6c167360ae8d5c1b82f7f08
Yinh27 <air12735@naver.com>
2023-09-12 00:43:46 +0900

    File Load Modification

--- fileWindow.py
+++ fileWindow.py
@@ -10,23 +10,55 @@ class FileWindow(QDialog, QWidget, form_fileWindow):
     def __init__(self, contents):
         super(FileWindow, self).__init__()
         self.file_contents = contents
+        self.lines = self.file_contents.split('\n')
         
         self.InitUI()
         self.show()
 
     def InitUI(self):
         self.setupUi(self)
+        self.srt_line = 0
+        self.fin_line = 20
         
-        self.textBrw.setPlainText(self.file_contents)
+        self.textBrw.setPlainText('\n'.join(self.lines[self.srt_line:self.fin_line]))
         self.searchButton.clicked.connect(self.search_file_contents)
+        self.prevButton.clicked.connect(self.prev_text)
+        self.nextButton.clicked.connect(self.next_text)
+        
+    def next_text(self):
+        if not (self.fin_line + 20) > len(self.lines):
+            self.srt_line += 20
+            self.fin_line += 20
+            self.textBrw.setPlainText('\n'.join(self.lines[self.srt_line:self.fin_line]))
+        elif (self.srt_line + 20) > len(self.lines):
+            self.srt_line += 20
+            self.textBrw.setPlainText('\n'.join(self.lines[self.srt_line:]))
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("[!] End of File")
+            msg.setWindowTitle("Alert")
+            msg.exec_()
+            
+    def prev_text(self):
+        if not (self.srt_line - 20) < 0:
+            self.srt_line -= 20
+            self.fin_line -= 20
+            self.textBrw.setPlainText(str(self.lines[self.srt_line:self.fin_line]))
+        else:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Information)
+            msg.setText("[!] Beginning of File")
+            msg.setWindowTitle("Alert")
+            msg.exec_()
 
     def search_file_contents(self):
         keyword = self.searchLine.text()
         if keyword:
             pattern = re.compile(re.escape(keyword), re.IGNORECASE)
             matched_text = ""
-            lines = self.file_contents.split('\n')
-            for line in lines:
+            
+            for line in self.lines:
                 if pattern.search(line):
 
                     line = pattern.sub(r'<span style="background-color: yellow;">\g<0></span>', line)
